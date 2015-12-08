112f0c27b73fea90fa56a68719d851ca8300787d
Clarence Pearson <clarence.pearson3@gmail.com>
2015-12-08 06:18:19 +0000

    Created mountaineer theme. Set as default. Lets Go WVU

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 /* File: gulpfile.js */
 
+// choose theme
+var theme = 'mountaineer';
+
 // source and destination helper functions
 function projectSource(p) {
   return 'src/'+p;
@@ -35,20 +38,18 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-// build detfault theme
-gulp.task('default-theme', function() {
-  return gulp.src(projectSource('themes/default.scss'))
-    .pipe(gulp.dest(projectSource('scss/_theme.scss')));
-})
 
-gulp.task('default-theme', function() {
-  return gulp.src(projectSource('themes/alt.scss'))
-    .pipe(gulp.dest(projectSource('scss/_theme.scss')));
-})
+// build _theme.scss
+gulp.task('build-theme', function() {
+  return gulp.src(projectSource('scss/themes/'+theme+'/*.scss'))
+  .pipe(concat('_theme.scss'))
+  .pipe(gulp.dest(projectSource('scss')));
+  
+});
 
 // sass task
-gulp.task('build-css', ['default-theme'], function() {
-  return gulp.src(projectSource('scss/**/*.scss'))
+gulp.task('build-css', function() {
+  return gulp.src(projectSource('scss/*.scss'))
     .pipe(sass({
       // include normalize
       includePaths: sassPaths
@@ -77,13 +78,12 @@ gulp.task('html', function() {
 });
 
 // build task
-gulp.task('build', ['jshint', 'build-js', 'build-css', 'html']);
+gulp.task('build', ['jshint', 'build-js', 'build-theme', 'build-css', 'html']);
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', ['build'], function() {
   gulp.watch('src/javascript/**/*.js', ['jshint', 'build-js']);
-  gulp.watch('src/scss/**/*.scss', ['build-css']);
+  gulp.watch('src/scss/*.scss', ['build-css']);
   gulp.watch('src/*.html', ['html']);
+  gulp.watch('src/scss/themes/'+theme+'/*.scss', ['build-theme']);
 });
-
-
