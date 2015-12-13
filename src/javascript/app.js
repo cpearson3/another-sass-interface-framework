d008b205999765055e07077c0d36d62fc61701b0
Clarence Pearson <clarence.pearson3@gmail.com>
2015-12-13 18:02:58 +0000

    Added jquery requirement to js file. Added nav init code

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,1 +1,15 @@
-// bootsmooth javascript support
+/* Bootsmooth App Init */
+
+$(function() {
+    
+    // initialize show menu buttons
+    var showMenu = $('.show-menu');
+    if (showMenu.length > 0) {
+        showMenu.on('click', function(e) {
+            e.preventDefault();
+            console.log(e);
+            $(e.target.hash).slideToggle();
+        });
+    }
+    
+});
